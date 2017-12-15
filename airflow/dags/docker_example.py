a15f13c2be3c067fd2efd06983a99dadbc6e3367
Eddie <eddie.santos.3@gmail.com>
2017-12-14 21:52:47 -0800

    project mvp

---
+++ airflow/dags/docker_example.py
@@ -0,0 +1,40 @@
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.docker_operator import DockerOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'docker_example', default_args=default_args, schedule_interval=timedelta(1))
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='docker_pull',
+    bash_command='docker pull esantos3/my-project:latest',
+    dag=dag)
+
+t2 = DockerOperator(
+    task_id='my_project',
+    image='esantos3/my-project:latest',
+    dag=dag)
+
+t2.set_upstream(t1)
---
+++ my-project/my-project.py
@@ -0,0 +1,19 @@
+from airflow.hooks import PostgresHook
+import os
+import pandas as pd
+
+hook = PostgresHook('postgres_default')
+conn = hook.get_conn()
+
+
+query = """
+SELECT 
+	  tpep_pickup_datetime
+	, passenger_count
+	, trip_distance
+	, total_amount
+FROM nyc_taxi
+LIMIT 10
+"""
+data = pd.read_sql(query, conn)
+print(data)
